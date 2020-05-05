51e19e4b1c5009c8b483fc45ae7095d9639863a7
MattT <trojanface@hotmail.com>
2020-05-05 12:39:17 +0930

    Get request for db.json now works

--- server.js
+++ server.js
@@ -5,18 +5,36 @@ const path = require("path");
 //Express server setup
 const app = express();
 let PORT = process.env.PORT || 3000;
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Will need a static file host command thing to make the public folder accessible
+
 //Routes
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+////GET
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/api/notes", function (req, res) {
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function (err, data) {
+    if (err) {
+      throw err;
+    } else {
+    //   console.log("test");
+    //   console.log(data);
+      return res.json(data);
+    }
   });
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
+});
+////POST
+app.post("/api/notes", function (req, res) {});
+////DELETE
+app.delete("/api/notes:id", function (req, res) {});
 
 //Start server
-app.listen(PORT, function() {
-    console.log("Listening on port "+ PORT);
-})
+app.listen(PORT, function () {
+  console.log("Listening on port " + PORT);
+});
