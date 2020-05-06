e210202686943292f15111a8f78e7b385f84da44
MattT <trojanface@hotmail.com>
2020-05-06 12:19:02 +0930

    Added a post and delete route for notes

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+//App variables
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+let notesArray = [];
 
 //Express server setup
 const app = express();
 let PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//Will need a static file host command thing to make the public folder accessible
+app.use(express.static("public"))
 
 //Routes
 ////GET
@@ -19,20 +20,45 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 app.get("/api/notes", function (req, res) {
-  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function (err, data) {
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function (err, data) {//turn this into a function and then call it from within a async await
     if (err) {
       throw err;
     } else {
-    //   console.log("test");
-    //   console.log(data);
+      notesArray = data;
       return res.json(data);
     }
   });
 });
 ////POST
-app.post("/api/notes", function (req, res) {});
+app.post("/api/notes", function (req, res) {
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function (err, data) {//turn this into a function and then call it from within a async await
+    if (err) {
+      throw err;
+    } else {
+       notesArray = JSON.parse(data);
+      let newNote = req.body;
+      notesArray.push(newNote);
+      return res.json(notesArray);
+    }
+  });
+});
 ////DELETE
-app.delete("/api/notes:id", function (req, res) {});
+app.delete("/api/notes:id", function (req, res) {
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function (err, data) {
+    if (err) {
+      throw err;
+    } else {
+      notesArray = JSON.parse(data);
+      notesArray.splice(parseInt(req.params.id),1);
+      fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notesArray), "utf8", function (err) {
+        if (err) {
+          throw err;
+        }
+      });
+      return res.json(notesArray);
+    }
+  });
+});
 
 //Start server
 app.listen(PORT, function () {
